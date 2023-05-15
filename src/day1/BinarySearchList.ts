21fe89d03b225eb604bf06d6a26b59ebd26187d8
Satyam <na20b060@smail.iitm.ac.in>
2023-05-16 00:30:24 +0900

    Solved TwoCrystalBalls (Linear Search and Binary Search)

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    
+    let lo = 0;
+    let hi = haystack.length;
 
+    do {
+        const m = Math.floor((lo+hi)/2);
+        const v = haystack[m];
+        if (v === needle){
+            return true;
+        }
+        else if (needle > v){
+            lo = m + 1;
+        }
+        else{
+            hi = m;
+        }
+
+    } while (lo<hi)
+    
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,38 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    const step = Math.floor(Math.sqrt(breaks.length));
+    let jump = step;   
+    for(; jump<breaks.length; jump += step){
+        if (breaks[jump]){
+            break
+        }
+    }
+    jump -= step;
+    for(let i = 0; i<jump && jump<breaks.length; i++, jump++){
+        if(breaks[jump]){
+            return jump;
+        }
+    }
+    return -1;
+
+    //If we want to make O (root N + root (root N) then we need to apply following code
+    // const step2 = Math.floor(Math.sqrt(step));
+    // for(let i = 0; i<jump && jump<breaks.length; i+=step2, jump+=step2){
+    //     if(breaks[jump]){
+    //         return jump;
+    //     }
+    // }
+    // return -1;
+
+    // First Attempt
+    // do{
+    //     const jp = jump*step;
+    //     const v = breaks[jp];
+    //     if (v === true){
+    //         const bp = jump*step;
+    //     } else{
+    //         jump++;
+    //     }   
+    // }while(jump*step<breaks.length);
+
 }
